7297e1f933a70be9080d1ed6061992fb25d5807c
stilistica <119519958+stilistica@users.noreply.github.com>
2023-05-11 16:44:21 +0300

    tasl_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+import getRandomHexColor from "./randomHexColor.js";
+
+const body = document.querySelector('body');
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
+
+let timer = null;
+
+startEl.addEventListener('click', beginChangeColor);
+stopEl.addEventListener('click', finishColorChange);
+
+function getColorBody() {
+	body.style.backgroundColor = getRandomHexColor();
+}
+
+function beginChangeColor() {
+	timer = setInterval(getColorBody, 1000);
+	startEl.disabled = true;
+}
+
+function finishColorChange() {
+	clearInterval(timer);
+	startEl.disabled = false;
+}
+
+
 
